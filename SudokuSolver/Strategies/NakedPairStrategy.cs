a1370259844274042faa910e48ce4fcb38e52dce
markmaire <54648631+markmaire@users.noreply.github.com>
2019-12-03 10:06:40 -0800

    Add files via upload

---
+++ SudokuSolver/Strategies/NakedPairStrategy.cs
@@ -0,0 +1,74 @@
+﻿using SudokuSolver.Workers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.Strategies
+{
+    class NakedPairStrategy : ISudokuStrategy
+    {
+
+        private readonly SudokuMapper _sudokuMapper;
+        public int[,] Solve(int[,] sudokuBoard)
+        {
+            for (int row = 0; row < sudokuBoard.GetLength(0); row++)
+            {
+                for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+                {
+                    EliminateNakedPairFromOthersInRow(sudokuBoard, row, col);
+                    EliminateNakedPairFromOthersInCol(sudokuBoard, row, col);
+                    EliminateNakedPairFromOthersInBlock(sudokuBoard, row, col);
+                }
+            }
+            return sudokuBoard;
+        }
+        private void EliminateNakedPairFromOthersInRow(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            if(!HasNakedPairInRow(sudokuBoard, givenRow, givenCol))
+            {
+                return;
+            }
+            for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+            {
+                if (sudokuBoard[givenRow, col] != sudokuBoard[givenRow, givenCol] && sudokuBoard[givenRow, col].ToString().Length > 1)
+                {
+                    EliminateNakedPair(sudokuBoard, sudokuBoard[givenRow, givenCol], givenRow, col);
+                }
+            }
+
+        }
+
+        private bool HasNakedPairInRow(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            for (int col = 0; col < sudokuBoard.GetLength(1); col++)
+            {
+                if(givenCol != col && IsNakedPair(sudokuBoard[givenRow, col], sudokuBoard[givenRow, givenCol]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool IsNakedPair(int firstPair, int secondPair)
+        {
+            return firstPair.ToString().Length == 2 && firstPair == secondPair;
+        }
+
+        private void EliminateNakedPairFromOthersInCol(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EliminateNakedPairFromOthersInBlock(int[,] sudokuBoard, int givenRow, int givenCol)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+    }
+}
---
+++ SudokuSolver/Workers/SudokuFileReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class SudokuFileReader
+    {
+        public int[,] ReadFile(string fileName)
+        {
+            int[,] sudokuBoard = new int[9, 9];
+            try
+            {
+                var sudokuBoardLines = File.ReadAllLines(fileName);
+                int row = 0;
+                foreach(var sudokuBoardLine in sudokuBoardLines)
+                {
+                    /*|1|2|3| |4| | |7|8|9|
+                     * ToArray would return _,1,2,3,_,4,_,6,7,8,9,_ a total of 11 elements while using Split
+                     * .Skip(1) will disregard the first "" which was a | and Take(9) will 'take' the next 9 disregarding last |/"" 
+                     */
+                    string[] sudokuBoardLineElements = sudokuBoardLine.Split('|').Skip(1).Take(9).ToArray();
+
+                    int col = 0;
+                    foreach (var sudokuBoardLineElement in sudokuBoardLineElements)
+                    {
+                        sudokuBoard[row, col] = sudokuBoardLineElement.Equals(" ") ? 0 : Convert.ToInt16(sudokuBoardLineElement);
+                        col++;
+                    }
+                    row++;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Something went wrong while reading the file: " + ex.Message);
+
+            }
+            return sudokuBoard;
+        }
+    }
+}
